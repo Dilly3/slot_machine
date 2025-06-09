e7a882c57ffecadc9c0410a35ba646bc490c634a
dilly3 <dil.anikamadu@gmail.com>
2025-06-09 03:13:40 +0100

    printColor function

--- food_truck.sh
+++ food_truck.sh
@@ -4,19 +4,43 @@ set -e
 set -o pipefail
 set -u
 
-
 printColor() {
-text=${1}
-color=${2}
-echo -e "\e[1;${color}m${text}\e[0m"
+    text=${1}
+    local -r color="${2^^}"
+    case $color in "RED")
+        echo -e "\e[1;31m${text}\e[0m"
+        ;;
+    "BLUE")
+        echo -e "\e[1;34m${text}\e[0m"
+        ;;
+    "YELLOW")
+        echo -e "\e[1;33m${text}\e[0m"
+        ;;
+    "GREEN")
+        echo -e "\e[1;32m${text}\e[0m"
+        ;;
+    "CYAN")
+        echo -e "\e[1;36m${text}\e[0m"
+        ;;
+    "ORANGE")
+        echo -e "\e[1;33m${text}\e[0m"
+        ;;
+    *) ;;
+    esac
+}
+
+terminate() {
+    msg=${1}
+    code=${2:-130}
+    printColor "${msg}" "red"
+    exit $code
+
 }
 
 welcome() {
-printColor "$(printf "\n%s\n" "===============================================")" "36"
-printColor "$(printf "%s\n" "=========Welcome to Food Truck ================")" "35"
+    printColor "$(printf "\n%s\n" "===============================================")" "Orange"
+    printColor "$(printf "%s\n" "=========Welcome to Food Truck ================")" "Cyan"
 }
 
 welcome
 exit 0
-
-
